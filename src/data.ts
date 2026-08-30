46d4dd8c72da4f83faf6d8033eeabb3c581bc060
alomdamohamed652-afk <alomdamohamed652@gmail.com>
2026-08-30 19:09:45 +0300

    fix: support product slugs in product model

--- src/data.ts
+++ src/data.ts
@@ -1,5 +1,5 @@
 export type Product = {
-  id: string; name: string; price: number; image: string; collection: string;
+  id: string; slug?: string; name: string; price: number; image: string; collection: string;
   sizes: string[]; colors: string[]; description: string; stock: number;
 };
 
